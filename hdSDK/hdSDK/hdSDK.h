1aa88a247d612b5904f63891251dae24c3c55fa2
Tran Cuong <trancaocuong99@gmail.com>
2022-08-30 14:48:12 +0700

    init

---
+++ hdSDK/hdSDK/hdSDK.h
@@ -0,0 +1,18 @@
+//
+//  hdSDK.h
+//  hdSDK
+//
+//  Created by mac on 30/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for hdSDK.
+FOUNDATION_EXPORT double hdSDKVersionNumber;
+
+//! Project version string for hdSDK.
+FOUNDATION_EXPORT const unsigned char hdSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hdSDK/PublicHeader.h>
+
+
